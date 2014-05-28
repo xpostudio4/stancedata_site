4d69e89e323ebb347cde5b18a3a6e93c5b37b5ee
Leonardo Jimenez <ljimenez@stancedata.com>
2014-05-28 00:31:06 -0400

    add new events app for news page. Modify .gitignore to avoid local database info when commiting. modify local settings to include the local.sqlite3 file

--- app/settings/local.py
+++ app/settings/local.py
@@ -19,7 +19,7 @@ if DEBUG == True:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-            'NAME': '',                      # Or path to database file if using sqlite3.
+            'NAME': 'local.sqlite3',                      # Or path to database file if using sqlite3.
             # The following settings are not used with sqlite3:
             'USER': '',
             'PASSWORD': '',
---
+++ events/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+
+class Event(models.Model):
+    title =  models.CharField(max_length=200)
+    from_date = models.DateField(blank=True)
+    to_date = models.DateField(blank=True)
+    description = models.TextField()
+    city = models.CharField(max_length=60)
+    country = models.CharField(max_length=100)
+
