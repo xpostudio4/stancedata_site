07ed0d3ba4687c827cc1c6664acb025786f35e61
Leonardo Jimenez <ljimenez@stancedata.com>
2014-06-14 23:45:21 -0400

    eliminated INSTALLED_APPS settings to avoid redundancy

--- app/settings/local.py
+++ app/settings/local.py
@@ -125,22 +125,7 @@ TEMPLATE_DIRS = (
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
     os.path.join(os.path.dirname(__file__),'..', 'templates'),
-)
-
-INSTALLED_APPS = (
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.sites',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    # Uncomment the next line to enable the admin:
-    'django.contrib.admin',
-    # Uncomment the next line to enable admin documentation:
-    # 'django.contrib.admindocs',
-    'zinnia',
-    'events',
-)
+    )
 
 SESSION_SERIALIZER = 'django.contrib.sessions.serializers.JSONSerializer'
 
