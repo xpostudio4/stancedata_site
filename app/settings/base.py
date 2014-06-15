20bab965ce6bfc47cc92b4f169f6d84ae1528e49
Leonardo Jimenez <ljimenez@stancedata.com>
2014-06-14 23:43:24 -0400

    add INSTALLED_APPS setting to base.py

--- app/settings/base.py
+++ app/settings/base.py
@@ -6,3 +6,18 @@ EMAIL_HOST_USER = 'ljimenez@stancedata.com'
 EMAIL_HOST_PASSWORD = 'Jesusvictor1'
 EMAIL_PORT = 587
 
+INSTALLED_APPS = (
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.sites',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    # Uncomment the next line to enable the admin:
+    'django.contrib.admin',
+    # Uncomment the next line to enable admin documentation:
+    # 'django.contrib.admindocs',
+    'django.contrib.sitemaps',
+    'zinnia',
+    'tagging',
+)
--- app/settings/production.py
+++ app/settings/production.py
@@ -118,21 +118,7 @@ TEMPLATE_DIRS = (
      os.path.join(os.path.dirname(__file__), '..', 'templates'),
 )
 
-INSTALLED_APPS = (
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.sites',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    # Uncomment the next line to enable the admin:
-    'django.contrib.admin',
-    # Uncomment the next line to enable admin documentation:
-    # 'django.contrib.admindocs',
-    'django.contrib.sitemaps',
-    'zinnia',
-    'tagging',
-)
+
 
 #zinnia spam filter options
 ZINNIA_SPAM_CHECKER_BACKENDS = ('zinnia.spam_checker.backends.automattic',)
